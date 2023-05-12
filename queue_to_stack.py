23a6b5941597be4bc72dae7c6b438deb8734af99
niuta24 <syvkova.pn@ucu.edu.ua>
2023-05-12 12:24:40 +0300

    Adding files

---
+++ queue_to_stack.py
@@ -0,0 +1,11 @@
+"""
+Queue to stack converter.
+"""
+
+from arrayqueue import ArrayQueue    # or from linkedqueue import LinkedQueue
+from arraystack import ArrayStack    # or from linkedstack import LinkedStack
+
+
+def queue_to_stack(queue):
+    """A queue to stack converter."""
+    
