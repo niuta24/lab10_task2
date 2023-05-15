6704f870749589cefc971458177214a8751616a3
niuta24 <syvkova.pn@ucu.edu.ua>
2023-05-15 16:26:12 +0300

    Final commit

--- queue_to_stack.py
+++ queue_to_stack.py
@@ -4,8 +4,13 @@ Queue to stack converter.
 
 from arrayqueue import ArrayQueue    # or from linkedqueue import LinkedQueue
 from arraystack import ArrayStack    # or from linkedstack import LinkedStack
-
+import copy
 
 def queue_to_stack(queue):
     """A queue to stack converter."""
-    
+    stack = ArrayStack()
+    change_queue = copy.deepcopy(queue)
+    for i in range(len(change_queue)):
+        stack.push(change_queue.peek())
+        change_queue.pop()
+    return stack
--- stack_to_queue.py
+++ stack_to_queue.py
@@ -4,34 +4,13 @@ Stack to queue converter.
 
 from arraystack import ArrayStack   # or from linkedstack import LinkedStack
 from arrayqueue import ArrayQueue   # or from linkedqueue import LinkedQueue
-
+import copy
 
 def stack_to_queue(stack):
     """A stack to queue converter.
     """
     queue = ArrayQueue()
-    
-    for i in range(len(stack)):
-        queue.add(stack.pop())
-    
+    change_stack = copy.deepcopy(stack)
+    for i in range(len(change_stack)):
+        queue.add(change_stack.pop())
     return queue
-if __name__ == "__main__":
-    stack = ArrayStack()
-    for i in range(10):
-        stack.add(i)
-    print(stack)
-    queue = stack_to_queue(stack)
-    print(queue)
-    # # [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
-    print(stack)
-    # # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    # print(stack.pop())
-    # # 9
-    # print(queue.pop())
-    # # 9
-    # stack.add(11)
-    # queue.add(11)
-    # print(queue)
-    # # [8, 7, 6, 5, 4, 3, 2, 1, 0, 11]
-    # print(stack)
-    # # [0, 1, 2, 3, 4, 5, 6, 7, 8, 11]
